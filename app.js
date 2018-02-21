1dcd8f8a08bf78e27593daec7bab7cbee6fa31b5
GummiWummiBear <19729435+GummiWummiBear@users.noreply.github.com>
2018-02-21 08:12:55 +0000

    Update app.js

--- app.js
+++ app.js
@@ -1,9 +1,1 @@
-const settings = require("./settings.json")
-const c=new (require("discord.js")).Client();
-c.on("ready",()=>{console.log("hi i'm alive\nhi alive i'm bot")});
-c.on("message",m=>{
-	if(/im/gi.test(m)){
-		m.channel.send(`hi ${m.content.replace(/'/g,"").split(/im\s(.+)/)[1]}, i'm ${c.user.username}`)
-	}
-});
-c.login(settings.token);
+const c=new (require("discord.js")).Client();c.on("ready",()=>{console.log("hi i'm alive\nhi alive i'm bot")});c.on("message",m=>{if(/im/gi.test(m)){m.channel.send(`hi ${m.content.replace(/'/g,"").split(/im\s(.+)/)[1]}, i'm ${c.user.username}`)}});c.login(require("./settings.json").token);
